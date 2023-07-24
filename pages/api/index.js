c2ed51717743bd6a2fd65df07e6c6b8a895bd5fa
Ruben Stein <ruben.stein@web.de>
2023-07-24 15:32:26 +0200

    verschlimmbessern

---
+++ pages/api/index.js
@@ -0,0 +1,11 @@
+import Heading from "../components/Heading";
+import SubHeading from "../components/SubHeading";
+
+export default function Home() {
+  return (
+    <main>
+      <Heading>🏄‍♂️🏄‍♀️🏄 Ruben´s geniale App 🏄🏄‍♀️🏄‍♂️</Heading>
+      <SubHeading>Rubens geniale App</SubHeading>
+    </main>
+  );
+}
